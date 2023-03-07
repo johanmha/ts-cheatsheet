5eb5098fd4b2c50b6350aca9ae2d03adf66740ac
Johan Haugland <johanhaugl@gmail.com>
2023-03-07 16:33:22 +0100

    types

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,8 @@
-let age: number = 20;
-if (age < 50)
-    age+=10;
-    console.log(age);
+let sales: number = 123_456_789;
+let courser: string = 'TypeScript';
+let is_published: boolean = true;
+let level;
+
+function render(document) {
+    console.log(document)
+}
