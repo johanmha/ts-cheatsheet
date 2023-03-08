ef7eac9ddf72d205e01c24c4444e10b0b0a73b7d
Johan Haugland <johanhaugl@gmail.com>
2023-03-08 09:08:54 +0100

    objects

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
-function calculateTax(income: number, taxYear: number = 2022) {
-    if (taxYear < 2022)
-        return income * 1.2
-    return income * 1.3;
-}
-
-calculateTax(10_000, 2023)
+let employee: {
+    readonly id: number,
+    name: string
+    retire: (date: Date) => void;
+} = { 
+    id: 1,
+    name: 'Johan',
+    retire: (date: Date) => {
+        console.log(date)
+    }
+};
