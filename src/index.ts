042f558b90ebf9e24906f33a02b48d1ddcbcae71
Johan Haugland <johanhaugl@gmail.com>
2023-03-08 10:39:37 +0100

    optional chaining

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,16 @@
-function greet(name: string | null | undefined) {
-    if (name) console.log(name.toUpperCase);
-    else console.log('Hola');
+type Customer = { birthday?: Date };
+function getCustomer(id: number): Customer | null | undefined {
+    return id === 0 ? null : { birthday: new Date() };
 }
 
-greet(null);
+let customer = getCustomer(1);
+
+// Optional propery access operator
+console.log(customer?.birthday?.getFullYear);
+
+//optional element access operator
+// customers?.[0]
+
+//optional call
+let log: any = null;
+log?.('a');
