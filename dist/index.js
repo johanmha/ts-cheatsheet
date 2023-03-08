a08cf337291b601b9161b4ac56b42c0dffb3f74d
Johan Haugland <johanhaugl@gmail.com>
2023-03-08 14:23:48 +0100

    nullish coaleshing operator

--- dist/index.js
+++ dist/index.js
@@ -1,10 +1,6 @@
 "use strict";
-var _a;
-function getCustomer(id) {
-    return id === 0 ? null : { birthday: new Date() };
-}
-let customer = getCustomer(1);
-console.log((_a = customer === null || customer === void 0 ? void 0 : customer.birthday) === null || _a === void 0 ? void 0 : _a.getFullYear);
-let log = null;
-log === null || log === void 0 ? void 0 : log('a');
+let speed = null;
+let ride = {
+    speed: speed !== null && speed !== void 0 ? speed : 30
+};
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,6 @@
-type Customer = { birthday?: Date };
-function getCustomer(id: number): Customer | null | undefined {
-    return id === 0 ? null : { birthday: new Date() };
-}
-
-let customer = getCustomer(1);
-
-// Optional propery access operator
-console.log(customer?.birthday?.getFullYear);
-
-//optional element access operator
-// customers?.[0]
-
-//optional call
-let log: any = null;
-log?.('a');
+let speed: number | null = null;
+let ride = {
+    // Falsy (undefined, null, '', false, 0)
+    // Nullish coalescing operator (not null or undefined)
+    speed: speed ?? 30
+};
