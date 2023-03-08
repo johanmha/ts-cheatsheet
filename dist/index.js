c2620cd344e5ebe93905c6b7ab29231872f01d27
Johan Haugland <johanhaugl@gmail.com>
2023-03-08 09:19:38 +0100

    union type

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,8 @@
 "use strict";
-let mySize = 2;
-console.log(mySize);
+function kgToLbs(weight) {
+    if (typeof weight === 'number')
+        return weight * 2.2;
+    else
+        return parseInt(weight) * 2.2;
+}
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,6 @@
-type Employee = {
-    readonly id: number,
-    name: string
-    retire: (date: Date) => void;
+function kgToLbs(weight: number | string): number {
+    if (typeof weight === 'number')  
+        return weight * 2.2;
+    else 
+        return parseInt(weight) * 2.2;
 }
-
-let employee: {
-} = { 
-    id: 1,
-    name: 'Johan',
-    retire: (date: Date) => {
-        console.log(date)
-    }
-};
