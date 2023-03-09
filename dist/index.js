d69355e6bc158d2caeb72c59660c4d4dd88abce9
Johan Haugland <johanhaugl@gmail.com>
2023-03-09 09:47:24 +0100

    Generic interfaces

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,9 @@
 "use strict";
-let phone = document.getElementById('phone');
-phone.value;
+var _a;
+function fetch(url) {
+    console.log('url: ', url);
+    return { data: null, error: null };
+}
+let result = fetch('url');
+(_a = result.data) === null || _a === void 0 ? void 0 : _a.title;
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,23 @@
-function wrapInArray<T>(value: T) {
-    return [value];
+// http://mywebsite.com/users
+// http://mywebsite.com/products
+
+interface Result<T> {
+    data: T | null;
+    error: string | null;
 }
 
-let numbers = wrapInArray(1);
+function fetch<T>(url: string): Result<T> {
+    console.log('url: ', url);
+    return { data: null, error: null };
+}
+
+interface User {
+    user: string;
+}
+
+interface Product {
+    title: string;
+}
+
+let result = fetch<Product>('url');
+result.data?.title;
